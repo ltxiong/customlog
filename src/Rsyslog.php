04a365fbe112be374e7457f060dd221b0d925955
ltxiong <ltx851201@yeah.net>
2020-05-18 15:32:59 +0800

    添加注释

--- src/Rsyslog.php
+++ src/Rsyslog.php
@@ -2,9 +2,11 @@
 
 namespace Ltxiong\CustomLog;
 
-
 /**
- * @desc 远程写 rsyslog 日志类，单次发送最大可发送4K数据 使用示例 TESTLOG|APILOG|SERVICELOG
+ * 远程写 rsyslog 日志类，单次发送最大可发送4K数据 使用示例 TESTLOG|APILOG|SERVICELOG
+ *   TESTLOG 研发代码过程输出的测试日志和调试日志，主要用于代码当中通过过程日志进行问题排查
+ *   APILOG api接口日志
+ *   SERVICELOG service服务日志
  * @example
  * 
  * use Ltxiong\CustomLog\Rsyslog;
@@ -77,22 +79,29 @@ class Rsyslog
     );
 
     /**
-     * @desc  构造函数 可输入策略数组，定义内容
-     * @access public
-     * @param void
-     * @return void
+     * 构造函数 可输入策略数组，定义内容
+     *
+     * @param string $host 日志服务器IP
+     * @param integer $port 日志服务器接收数据的端口
+     * @param array $log_id_arr  自定义日志前缀数组
      */
-    public function __construct($host, $port, $log_id_arr)
+    public function __construct($host = '', $port = 19880, $log_id_arr = array())
     {
         $this->_host = $host;
         $this->_port = $port;
 		$this->_log_id_arr = $log_id_arr;
     }
 
+
     /**
-     * 发送消息之前，进行消息前置处理，包括消息体长度处理
+     * 发送消息之前，进行消息前置处理，包括消息体长度处理，如消息内容超过最大长度，将对消息内容进行截断只有前N长度的内容有效
+     *
+     * @param string $message 日志消息内容
+     * @param string $msg_prefix 日志消息前缀 
+     * @param integer $msg_len 消息长度 
+     * @return void
      */
-    private function ProcessMessage($message, $msg_prefix, $msg_len = 1024)
+    private function ProcessMessage($message = '', $msg_prefix = '', $msg_len = 1024)
     {
         if(empty($message) || !is_string($message))
         {
@@ -104,12 +113,16 @@ class Rsyslog
         $log_send_time = intval(microtime(true) * 1000);
         // 消息最前面带空格，要么消息最前面带上<>开头和结尾的特殊字符，
         // 例如： <PHP RSYSLOG>Apr BLOG_LTX3_KKK 1xxxxxxxxxx 22020/04/20/11:32:43，实际消息内容为 1xxxxxxxxxx 22020/04/20/11:32:43
-        $message = substr("<PHP RSYSLOG>" . date("F j Y g:i a") . " $msg_prefix $log_send_time $message", 0, $msg_len);
+        $message = substr($message, 0, $msg_len);
+        $message = "<PHP RSYSLOG>" . date("F j Y g:i a") . " $msg_prefix $log_send_time $message";
         return $message;
     }
 
     /**
      * 发送消息之前，进行消息前缀处理
+     *
+     * @param integer / integer numberical $log_id 日志ID(根据一定的规范命名)
+     * @return string 返回日志ID对应的消息前缀
      */
     private function GetMessagePrefix($log_id)
     {
@@ -121,7 +134,13 @@ class Rsyslog
     }
 
     /**
-     * 发送消息到远程rsyslog 服务器
+     * 通过TCP/UDP协议 发送消息到远程rsyslog 服务器，并能够进行日志归类和自动分目录存储
+     *
+     * @param string $message 日志消息内容
+     * @param integer / integer numberical $log_id 日志ID(根据一定的规范命名)
+     * @param integer $msg_len  消息长度，默认1024字节，最大4096字节
+     * @param string $socket_transport 传输协议，默认为tcp协议
+     * @return void
      */
     public function Send($message, $log_id, $msg_len = 1024, $socket_transport = 'tcp')
     {
@@ -159,8 +178,9 @@ class Rsyslog
 
 }
 
+
 /**
- * // 使用示例 TESTLOG|APILOG|SERVICELOG
+ * 
  * $log_id_arr = array(1000001 => 'TESTLOG_HYPERF_TT', 1000002 => 'APILOG_FIND_YY', 1000003 => 'SERVICELOG_FIND_YY',);
  * $message = json_encode($data);
  * $rsyslog_instance = new Rsyslog('133.10.0.11', 19858, $log_id_arr);
